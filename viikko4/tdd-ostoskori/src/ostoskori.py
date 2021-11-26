03019cecd76fa1b2477cdaf77cc20a7f31b95d0e
karhelmi <helmi.karesti@helsinki.fi>
2021-11-26 15:45:03 +0200

    vko4 TDD-tehtävä #11: Kaksi samaa tuotetta lisätään ja ostoskori sisältää yhden ostoksen

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -9,8 +9,12 @@ class Ostoskori:
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        self.tavaroita_korissa = len(self.ostoskori)
-        return self.tavaroita_korissa
+        tavaramaara = 0
+        for ostos in self.ostoskori:
+            tavaramaara += ostos.lukumaara()
+        return tavaramaara
+        #self.tavaroita_korissa = len(self.ostoskori)
+        #return self.tavaroita_korissa
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
@@ -26,8 +30,16 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        lisatty_ostosolio = self.ostos(lisattava)
-        self.ostoskori.append(lisatty_ostosolio)
+        if len(self.ostoskori) > 0:
+            for ostos in self.ostoskori:
+                if ostos.tuotteen_nimi() == lisattava.nimi():
+                    ostos.muuta_lukumaaraa(1)
+                    return
+            lisattava_ostosolio = self.ostos(lisattava)
+            self.ostoskori.append(lisattava_ostosolio)
+        else:
+            lisattava_ostosolio = self.ostos(lisattava)
+            self.ostoskori.append(lisattava_ostosolio)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
