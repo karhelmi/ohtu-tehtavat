c75efbf6afe96ae156ca009ebba42c3336f7e041
karhelmi <helmi.karesti@helsinki.fi>
2021-12-10 17:01:47 +0200

    Viikko 6 tehtävät 1-3 (nelonen aloitettu vähän)

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,54 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
+
+
+class QueryBuilder:
+    def __init__(self):
+        self.query_olio = pass
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    #matcher = And(
+    #    HasAtLeast(5, "goals"),
+    #    HasAtLeast(5, "assists"),
+    #    PlaysIn("PHI")
+    #)
+
+    #matcher = And(
+    #    Not(HasAtLeast(1, "goals")),
+    #    PlaysIn("NYR")
+    #)
+
+    #matcher = And(
+    #    (HasFewerThan(1, "goals")),
+    #    PlaysIn("NYR")
+    #)
+
+    #matcher = All()
+
+    #matcher = Or(
+    #HasAtLeast(30, "goals"),
+    #HasAtLeast(50, "assists")
+    #)
+
+    #matcher = And(
+    #    HasAtLeast(40, "points"),
+    #    Or(
+    #        PlaysIn("NYR"),
+    #        PlaysIn("NYI"),
+    #        PlaysIn("BOS")
+    #    )
+    #)
+    query = QueryBuilder()
+    matcher = query.build()
+
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
